fae793be16e83ec9b11187907a14a47ae3d8d57c
Manuel Peter <manuel.peter@itk-engineering.de>
2023-12-07 08:46:22 +0100

    dropzone implementation (failing)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,22 @@ import Dropzone from 'dropzone';
 import 'dropzone/dist/dropzone.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
-// // Your Dropzone configuration here
+import $ from "jquery/dist/jquery.slim";
+
+// Your Dropzone configuration here
 // Dropzone.options.myDropzone = {
 //   url: '/file-upload', // your backend URL where the file will be uploaded
 // };
 
 // // Initialize the Dropzone
-// const myDropzone = new Dropzone("#my-dropzone");
+
+$(function() {
+    window.console.log("dom ready");
+
+    // //const myDropzone = new Dropzone("#my-dropzone");
+    // let myDropzone = new Dropzone("#my-dropzone");
+    // myDropzone.on("addedfile", file => {
+    // console.log(`File added: ${file.name}`);
+    // });
+    // window.console.log(myDropzone);
+  });
